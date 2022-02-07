b09679eaa0f482a335b70a01fbdaf57aae72de7d
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-07 15:44:11 +0300

    defined save delete and instances

--- insta/models.py
+++ insta/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 class Profile(models.Model):
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
@@ -7,7 +9,29 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, default="Caption", blank=True)    
     location = models.CharField(max_length=60, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
     
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
 class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     name = models.CharField(max_length=250, blank=True)
@@ -16,10 +40,31 @@ class Post(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
     created = models.DateTimeField(auto_now=True)
     
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.user.name} Post'
+
 
 class Comment(models.Model):
     comment = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
     created = models.DateTimeField(auto_now=True)
-    
+
+    def __str__(self):
+        return f'{self.follower} Follow'
+
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'
