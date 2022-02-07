3c7209b0a3f40d49e7ccdb76375f166afcd12318
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-07 14:05:41 +0300

    created models profile,comment, post

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    name = models.CharField(blank=True, max_length=120)
+    bio = models.TextField(max_length=500, default="Caption", blank=True)    
+    location = models.CharField(max_length=60, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True)
+    
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    name = models.CharField(max_length=250, blank=True)
+    caption = models.CharField(max_length=250, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    created = models.DateTimeField(auto_now=True)
+    
+
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now=True)
+    
