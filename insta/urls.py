c949e0fb75e409b0e94a3c57a94402dbf3632053
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-07 15:44:37 +0300

    added url for searches

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,6 @@
-from unicodedata import name
 from django.urls import path, include
 from . import views
-
 urlpatterns = [
     path('', views.index, name= 'index'),
-    
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
 ]
