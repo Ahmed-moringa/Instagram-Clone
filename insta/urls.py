c66bb349903718b34b3e9c3f22767e148fe5acf5
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-07 16:59:31 +0300

    added paths search profile and post

--- insta/urls.py
+++ insta/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('search/', views.search_profile, name='search'),
+    path('post/<id>', views.post_comment, name='comment'),
 ]
